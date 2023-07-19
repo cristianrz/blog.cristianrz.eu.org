e20cd89cc103af548cd44a0daa9e5e0b8c160d68
cristianrz <59292334+cristianrz@users.noreply.github.com>
2023-07-19 12:17:50 +0100

    Continue even if cp cant find anything

--- deploy.sh
+++ deploy.sh
@@ -9,10 +9,10 @@ rm "$DEST"/*
 for i in . article page; do
 	mkdir -p "${DEST}/${i}"
 
-	cp -v ./"$i"/*.html "${DEST}/${i}"
-	cp -v ./"$i"/*.xml "${DEST}/${i}"
+	cp -v ./"$i"/*.html "${DEST}/${i}" || true
+	cp -v ./"$i"/*.xml "${DEST}/${i}" ||  true
 done
 
-find "$DEST" -type f -exec chmod 0640 {} +
-find "$DEST" -type d -exec chmod 0750 {} +
+find "$DEST" -type f -exec chmod 0640 {} + 
+find "$DEST" -type d -exec chmod 0750 {} + 
 
